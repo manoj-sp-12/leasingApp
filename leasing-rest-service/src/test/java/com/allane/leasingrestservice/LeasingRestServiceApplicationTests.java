db9fb116348c9620534e8df515facf5aa535d727
SAKTHI PARAMESWARAN Manoj <manoj.sakthi@soprabanking.com>
2022-12-29 15:09:10 +0100

    initial project setup

---
+++ leasing-rest-service/src/test/java/com/allane/leasingrestservice/LeasingRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allane.leasingrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeasingRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
