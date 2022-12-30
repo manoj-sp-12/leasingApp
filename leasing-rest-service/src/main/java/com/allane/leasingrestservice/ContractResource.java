e20e22809538bfb3b9b8ef29e6b514035931e4c1
Manoj S P <manoj.sp@gmail.com>
2022-12-30 15:58:03 +0100

    get contacts list using rest webservice

--- leasing-rest-service/src/main/java/com/allane/leasingrestservice/ContractResource.java
+++ leasing-rest-service/src/main/java/com/allane/leasingrestservice/ContractResource.java
@@ -6,6 +6,7 @@ package com.allane.leasingrestservice;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ import com.allane.leasingrestservice.contract.ContractHardcodedService;
  * @author msakthi
  *
  */
-
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class ContractResource {
 	
--- leasing-rest-service/src/main/java/com/allane/leasingrestservice/contract/ContractHardcodedService.java
+++ leasing-rest-service/src/main/java/com/allane/leasingrestservice/contract/ContractHardcodedService.java
@@ -20,8 +20,8 @@ public class ContractHardcodedService {
 	private static int idCounter = 123566;
 	
 	static {
-		contracts.add(new Contract(++idCounter, "Max Mustermann", "BMW X3 (2022)", "X123456", "350,00E", "45.350,00E" ));
-		contracts.add(new Contract(++idCounter, "Maria Musterfrau", "BMW 330i (2022)", "-", "365,00E", "47.350,00E" ));
+		contracts.add(new Contract(++idCounter, "Max Mustermann", "BMW X3 (2022)", "X123456", "350", "45350" ));
+		contracts.add(new Contract(++idCounter, "Maria Musterfrau", "BMW 330i (2022)", "-", "365", "47350" ));
 	}
 	
 	public List<Contract> findAll() {
